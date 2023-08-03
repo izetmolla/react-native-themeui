8715bd45e40c4eb0c3e7bfc23afc1ac7a7a7c5fa
Izet Molla <imolla@uet.com>
2023-08-03 15:41:50 +0200

    new

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
 import Box from "./components/Box";
 import Text from "./components/Text";
 import ThemeUiProvider, { ThemeUiActionContext, ThemeUiContext } from "./contexts/ThemeUiProvider";
+import { useTheme } from "./hooks/useTheme";
 
-export { ThemeUiContext, ThemeUiActionContext, ThemeUiProvider, Box, Text }
+export { ThemeUiContext, ThemeUiActionContext, ThemeUiProvider, Box, Text, useTheme }
